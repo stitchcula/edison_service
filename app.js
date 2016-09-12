ad09201561571e6d4709300523469234093b324c
stitch cula <1132463097@qq.com>
2016-09-13 05:29:22 +0800

    created user modelue.

--- app.js
+++ app.js
@@ -2,9 +2,14 @@
  * Created by stitchcula on 2016/9/4.
  */
 
+import Koa from 'koa'
+import Router from 'koa-router'
+import mongo from 'koa-mongo'
+import convert from 'koa-convert'
 import mongodb from 'mongodb'
 import Mosca from './lib/cMosca.js'
 
+/***  MQTT init  ***/
 (async function () {
     const mongo=await mongodb.MongoClient
         .connect(`mongodb://${process.env['MONGO_HOST']}:${process.env['MONGO_PORT']}/smth`)
@@ -13,4 +18,41 @@ import Mosca from './lib/cMosca.js'
     mosca.listen(8005)
 })()
 
+/***  Router load  ***/
+const router=new Router()
+const routes=require('dir-requirer')(__dirname)('./routes')
+for(var r in routes){
+    router.use('/'+r,routes[r].router.routes())
+}
+
+/***  Koa init ***/
+const app=new Koa()
+app.use(convert(mongo({
+    host:process.env['MONGO_HOST']||'localhost',
+    port:process.env['MONGO_PORT']||27017,
+    //user:process.env['MONGO_USER']||"root",
+    //pass:process.env['MONGO_PWD']||"",
+    db:"smth"
+})))
+
+app.use(async (ctx,next)=>{
+    console.log(ctx.ip+" "+ctx.method+" "+ctx.path+" at "+new Date().toLocaleString())
+    ctx.mongo=ctx.mongo.db("smth")
+    try{
+        await next()
+    }catch(err){
+        console.error(err)
+        if(typeof(err)=='string'&&err.length==3)
+            ctx.body=err
+        else if(typeof(err)=='number'&&err.length==3)
+            ctx.status=err
+        else
+            ctx.body="500"
+    }
+})
+
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+app.listen(8007)
 
---
+++ routes/user.js
@@ -0,0 +1,70 @@
+/**
+ * Created by stitchcula on 2016/9/11.
+ */
+"use strict";
+
+import Router from 'koa-router'
+import crypto from 'crypto'
+import parse from 'co-body'
+
+const router=new Router()
+
+router.post('/create',async (ctx,next)=>{
+    var {account,email,pass}=await parse.json(ctx)
+    if(!(await ctx.mongo.collection("users").findOne({"$or":[{account},{email}]}))) {
+        var ituin
+        while(1){
+            ituin=parseInt(Math.random()*100000000).toString()
+            if(ituin.length==8&&
+                !(await ctx.mongo.collection("users").findOne({uin:ituin},{_id:0})))
+                break
+        }
+        var token=crypto.createHmac('sha1',ituin+new Date().getTime().toString()).digest('hex').substr(0,18)
+        await ctx.mongo.collection("users").insertOne({uin:ituin,account,email,pass,token})
+        ctx.body={result:{uin:ituin,token},err_code:null,err_msg:null}
+    }else{
+        ctx.body={result:{uin:null,token:null},err_code:595,err_msg:"already exists."}
+    }
+    await next()
+})
+
+router.get('/bind',async (ctx,next)=>{
+    var res=ctx.query.token?
+        await ctx.mongo.collection("users").findOne({token:ctx.query.token}):
+        await ctx.mongo.collection("users").findOne({device_id:ctx.query.device_id})
+    ctx.body=res?{result:{uin:res.uin,token:res.token,device_id:res.device_id}}:{err_code:502,err_msg:"denial of service."}
+    await next()
+})
+
+router.get('/',async (ctx,next)=>{
+    var res=ctx.query.token?
+        await ctx.mongo.collection("users").findOne({token:ctx.query.token,uin:ctx.query.uin}):
+        await ctx.mongo.collection("users").findOne({pass:ctx.query.pass,email:ctx.query.email})
+    ctx.body=res?{result:res}:{err_code:502,err_msg:"denial of service."}
+    await next()
+})
+router.put('/',async (ctx,next)=>{
+    var res=await ctx.mongo.collection("users").findOne({token:ctx.query.token,uin:ctx.query.uin})
+    if(!res)
+        return ctx.body={err_code:502,err_msg:"denial of service."}
+    var body=await parse.json(ctx)
+    if(body.uin) delete body.uin
+    if(body.token) delete body.token
+    delete res._id
+    await ctx.mongo.collection("users").updateOne({token:ctx.query.token,uin:ctx.query.uin}
+        ,{"$set":Object.assign(res,body)}, {upsert:true})
+    ctx.body={result:200}
+    await next()
+})
+router.del('/',async (ctx,next)=>{
+    var res=await ctx.mongo.collection("users").findOne({token:ctx.query.token,uin:ctx.query.uin})
+    if(!res)
+        return ctx.body={err_code:502,err_msg:"denial of service."}
+    var token=crypto.createHmac('sha1',ituin+new Date().getTime().toString()).digest('hex').substr(0,18)
+    await ctx.mongo.collection("users").updateOne({token:ctx.query.token,uin:ctx.query.uin}
+        ,{"$set":token}, {upsert:true})
+    ctx.body={result:200}
+    await next()
+})
+
+export {router}
